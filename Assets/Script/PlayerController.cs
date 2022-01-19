9d0866670ece8a16e5170f6b9ce55b1897350b1e
KBC12006160\kbc12006160 <ryu-ookouchi_20h@stu.kbc.ac.jp>
2022-01-19 14:52:47 +0900

    1カウントされると力の向きが反転する

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public SpeedGage SG;
+    public Level level;
     public int Move = 0;
-    
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
     void FixedUpdate()
     {
         Rigidbody rb = this.GetComponent<Rigidbody>();  // rigidbodyを取得
-        Vector3 force = new Vector3(Move* SG.GageAmount, 0.0f, 0.0f);    // 力を設定
-        rb.AddForce(force - rb.velocity, ForceMode.Impulse);  // 力を加える
+
+        if (level.runcount == 0 || level.runcount % 2 == 0)
+        {
+            Vector3 force = new Vector3(Move * SG.GageAmount, 0.0f, 0.0f);    // 力を設定
+            rb.AddForce(force - rb.velocity, ForceMode.Impulse);  // 力を加える
+        }
+        else
+        {
+            Vector3 force = new Vector3(-Move * SG.GageAmount, 0.0f, 0.0f);    // 力を設定
+            rb.AddForce(force - rb.velocity, ForceMode.Impulse);  // 力を加える
+        }
     }
 }
