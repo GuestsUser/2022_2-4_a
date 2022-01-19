a21fe8e98a328893a9c39ec2c1fd29a76263095e
OoishiRen <ren-ooishi-20h@stu.kbc.ac.jp>
2022-01-19 10:59:03 +0900

    ゲームオーバーの試験用スクリプトを入れました。

---
+++ Assets/Script/Game_Over.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public int miss_count;
+    public bool miss;
+    public bool game_over;  //ゲームオーバーフラグ
+    
+    void Start()
+    {
+        miss_count = 0;
+
+        miss = false;
+        game_over = false;  //フラグの初期化
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            miss = true;
+            if (miss)
+            {
+                if (miss_count >= 1)
+                {
+                    game_over = true;
+                }
+                miss_count++;
+                miss = false;
+            }
+        }
+
+            if (game_over)      //ゲームオーバーになったら↓
+            {
+            Debug.Log("リザルトに遷移");
+                //SceneManager.LoadScene("Result");
+            }
+    }
+}
