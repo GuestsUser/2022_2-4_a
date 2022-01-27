339b3dd413e9236a85553f052966b575bb7e68d5
KBC12006160\kbc12006160 <ryu-ookouchi_20h@stu.kbc.ac.jp>
2022-01-27 12:18:47 +0900

    警告表示できるようにした

--- Assets/Script/FlushController.cs
+++ Assets/Script/FlushController.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FlushController : MonoBehaviour
-{
-    public float speed = 1.0f;  //点滅させるスピード
-    public Level level;
-
-    private RawImage image;     //RawImageオブジェクト
-    private float time;         //時間
+{  
+    private float time;         //点滅時間
+    private const float X_Speed = 0.04f;    //横スクロールスピード
 
     public bool Blinking;       //点滅フラグ
     public bool Scroll;         //Warning横スクロールフラグ
+    public bool ReScroll;       //Warning逆横スクロールフラグ
+    public float speed = 1.0f;  //点滅させるスピード
 
-    private const float X_Speed = 0.04f;
-    public RawImage warning;
+    //参照
+    public Level level;         //レベル
+    public Game_Over gameover;  //ゲームオーバー
+    public RawImage warning;    //スクロールオブジェクト
+    private RawImage image;     //点滅オブジェクト
 
     void Start()
     {
@@ -47,6 +49,12 @@ public class FlushController : MonoBehaviour
         {
             var rect = warning.uvRect;
             rect.x = (rect.x + X_Speed * Time.unscaledDeltaTime) % 1.0f;
+            warning.uvRect = rect;
+        }
+        else if(ReScroll == true)
+        {
+            var rect = warning.uvRect;
+            rect.x = (rect.x - X_Speed * Time.unscaledDeltaTime) % 1.0f;
             warning.uvRect = rect;
         }
     }
